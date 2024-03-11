7db1b9b509ac962e64f659ac0a547931877f1fd1
arnabnandikgp <arnabnandi2002@gmail.com>
2024-03-11 18:57:00 +0530

    Fixes bug for Imaginary leadterm for _eval_nseries of Abs class
    Signed-off-by: arnabnandikgp <arnabnandi2002@gmail.com>

--- sympy/series/tests/test_limits.py
+++ sympy/series/tests/test_limits.py
@@ -1424,3 +1424,12 @@ def test_issue_22982_15323():
     assert limit((1 - 1/x)**x*(log(1 - 1/x) + 1/(x*(1 - 1/x))), x, 1, dir='+') == 1
     assert limit((log(E + 1/x) )**(1 - sqrt(E + 1/x)), x, oo) == 1
     assert limit((log(E + 1/x) - 1)**(- sqrt(E + 1/x)), x, oo) == oo
+
+
+def test_issue_25681():
+    x = Symbol('x')
+    expr = x/abs(sqrt(x**2 - 1))
+    assert limit(expr, x, 1, '+') == oo
+    assert limit(expr, x, -1, '-') == -oo
+    assert limit(expr, x, 1, '-') == oo
+    assert limit(expr, x, -1, '+') == -oo
